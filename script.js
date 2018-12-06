f4eb05f9df0ccfed970fbd49100f1cdb0e501c42
Emily Pitts <emilypitts@Emilys-MacBook-Pro-2.local>
2018-12-05 18:07:51 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+classesObject = {
+  TAC300: {
+    title: 'Intro to Computer Science',
+    day: "Wed",
+    time: "6:00pm",
+  },
+  TAC120: {
+    title: 'Production Techniques in Logic Pro',
+    day: "Tues",
+    time: "3:00pm",
+  },
+  TAC220: {
+    title: "Into to Sound Design",
+    day: "Fri",
+    time: "10:00am",
+  }
+}
+console.log('obj', classesObject);
+
+classesArray = [
+  {
+    id: 'TAC300',
+    title: 'Intro to Computer Science',
+    day: "Wed",
+    time: "6:00pm",
+  },
+  {
+    id: 'TAC120',
+    title: 'Production Techniques in Logic Pro',
+    day: "Tues",
+    time: "3:00pm",
+  },
+  {
+    id: 'TAC220',
+    title: "Into to Sound Design",
+    day: "Fri",
+    time: "10:00am",
+  }
+];
+console.log('array', classesArray);
